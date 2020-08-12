87dc69b3157f3df99b0f244969d04ae4b8af43b1
fahadmak <f.faraqhan@gmail.com>
2020-08-12 12:04:04 -0500

    first commit

---
+++ src/main/java/edu/miu/intergration_sys/IntergrationSysApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.intergration_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergrationSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntergrationSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/intergration_sys/IntergrationSysApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.intergration_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntergrationSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
