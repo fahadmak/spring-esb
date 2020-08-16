1e84267b5c67edc54f2fbe9355a9a3eb7b28f416
fahadmak <f.faraqhan@gmail.com>
2020-08-15 21:13:14 -0500

    Set up ampq

--- src/main/java/edu/miu/intergration_sys/IntergrationSysApplication.java
+++ src/main/java/edu/miu/intergration_sys/IntergrationSysApplication.java
@@ -1,13 +1,17 @@
 package edu.miu.intergration_sys;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class IntergrationSysApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IntergrationSysApplication.class, args);
+		new SpringApplicationBuilder(IntergrationSysApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
 	}
 
 }
